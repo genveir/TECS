5149423cd63d5ef423c9515f3d87596fa544b0a7
Geerten Vink <maedaros@yahoo.com>
2023-08-20 12:20:49 +0200

    rename factory, it builds blueprints, not chips

---
+++ TECS.HDLSimulator/Chips/ChipBlueprintFactory.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TECS.HDLSimulator.Chips;
+
+public class ChipBlueprintFactory
+{
+    private readonly IEnumerable<ChipSummary> _summaries;
+    private readonly Dictionary<string, ChipBlueprint> _blueprints = new();
+
+    public ChipBlueprintFactory(IEnumerable<ChipSummary> summaries)
+    {
+        _summaries = summaries;
+    }
+
+    public void BuildBlueprints()
+    {
+        
+    }
+
+    private ChipBlueprint NandBlueprint()
+    {
+        var nandNode = new NandNode();
+
+        nandNode.TryGetInputPins(out var inputNodes);
+        
+        var inputs = new Dictionary<string, NandPinNode>
+        {
+            { "a", inputNodes.a },
+            { "b", inputNodes.b }
+        };
+
+        return new ChipBlueprint(inputs, "out", nandNode);
+    }
+}
