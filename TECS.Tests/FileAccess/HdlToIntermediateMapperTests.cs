9c46df7c6af9cfa9a531d6b97fbf35da9d2eb131
Geerten Vink <maedaros@yahoo.com>
2023-08-24 17:59:35 +0200

    more small things

--- TECS.Tests/FileAccess/HdlToIntermediateMapperTests.cs
+++ TECS.Tests/FileAccess/HdlToIntermediateMapperTests.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using System.IO;
 using FluentAssertions;
 using NUnit.Framework;
 using TECS.DataIntermediates.Chip;
-using TECS.DataIntermediates.Chip.Names;
 using TECS.FileAccess;
 using TECS.FileAccess.FileAccessors;
 using TECS.FileAccess.Mappers;
@@ -14,23 +12,33 @@ namespace TECS.Tests.FileAccess;
 public class HdlToIntermediateMapperTests
 {
     [Test]
-    public void CanParseRegularNot()
+    public void CanMapRegularNot()
     {
         var notHdl = Get("Not");
 
         var intermediate = HdlToIntermediateMapper.Map(notHdl);
 
-        intermediate.Should().BeEquivalentTo(ParsedNotIntermediate);
+        intermediate.Should().BeEquivalentTo(NotIntermediate);
     }
 
     [Test]
-    public void CanParseWeirdNot()
+    public void CanMapRegularAnd()
+    {
+        var andHdl = Get("And");
+
+        var intermediate = HdlToIntermediateMapper.Map(andHdl);
+
+        intermediate.Should().BeEquivalentTo(AndIntermediate);
+    }
+
+    [Test]
+    public void CanMapWeirdNot()
     {
         var weirdNotHdl = Get("WeirdNot");
 
         var intermediate = HdlToIntermediateMapper.Map(weirdNotHdl);
 
-        intermediate.Should().BeEquivalentTo(ParsedWeirdNotIntermediate);
+        intermediate.Should().BeEquivalentTo(WeirdNotIntermediate);
     }
 
     private HdlFile Get(string name)
@@ -43,7 +51,7 @@ public class HdlToIntermediateMapperTests
                throw new FileNotFoundException($"{name} hdl does not exist but is required for test");
     }
 
-    private static ChipData ParsedNotIntermediate => new ChipDataBuilder()
+    private static ChipData NotIntermediate => new ChipDataBuilder()
         .WithName("Not")
         .WithInGroups("in")
         .WithOutGroups("out")
@@ -54,8 +62,23 @@ public class HdlToIntermediateMapperTests
             .Build()
         .Build();
 
-    private static ChipData ParsedWeirdNotIntermediate => new ChipDataBuilder()
-        .WithName("Not")
+    private static ChipData AndIntermediate => new ChipDataBuilder()
+        .WithName("And")
+        .WithInGroups("a", "b")
+        .WithOutGroups("out")
+        .AddPart("Nand")
+            .AddLink("a", "a")
+            .AddLink("b", "b")
+            .AddLink("out", "mid")
+            .Build()
+        .AddPart("Not")
+            .AddLink("in", "mid")
+            .AddLink("out", "out")
+            .Build()
+        .Build();
+
+    private static ChipData WeirdNotIntermediate => new ChipDataBuilder()
+        .WithName("NotWeird")
         .WithInGroups("in")
         .WithOutGroups("out", "out2")
         .AddPart("Nand")
